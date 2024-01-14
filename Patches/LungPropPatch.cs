95121aa407a2753007a176992f4b968128505747
Fexlar <122419606+fexlars@users.noreply.github.com>
2024-01-14 10:51:10 -0600

    first commit

---
+++ Patches/LungPropPatch.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Collections.Concurrent;
+
+namespace OneshotSunApparatice.Patches
+{
+    [HarmonyPatch(typeof(LungProp))]
+    internal class LungPropPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void ReplaceModel(LungProp __instance)
+        {
+            Plugin.Instance.mls.LogInfo("LungPropPatch.Start");
+
+            GameObject.Destroy(__instance.gameObject.transform.Find("Mesh").gameObject);
+            GameObject gameObject = GameObject.Instantiate(Plugin.assetBundle.LoadAsset<GameObject>("assets/bulb.prefab"), __instance.gameObject.transform);
+            // Hard Coded Positioning because I'm lazy
+            gameObject.transform.localRotation = Quaternion.Euler(0, 180, 180);
+            gameObject.transform.localScale = new Vector3(4, 4, 4);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace OneshotSunApparatice
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        public static Plugin Instance;
+        internal ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_NAME);
+        internal static AssetBundle assetBundle;
+
+        // Stolen Code from SharedUtils, sorry...
+        public static string PathForResourceInAssembly(string resourceName, Assembly assembly = null)
+        {
+            return Path.Combine(Path.GetDirectoryName((assembly ?? Assembly.GetCallingAssembly()).Location), resourceName);
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                harmony.PatchAll();
+            }
+
+            string FolderLocation = Instance.Info.Location;
+            assetBundle = AssetBundle.LoadFromFile(PathForResourceInAssembly("bulb", null));
+            if (assetBundle == null)
+            {
+                mls.LogInfo("Failed to load AssetBundle!");
+                return;
+            }
+            else
+            {
+                mls.LogInfo("AssetBundle loaded!");
+            }
+
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(Patches.LungPropPatch));
+
+            // Plugin startup logic
+            mls.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
