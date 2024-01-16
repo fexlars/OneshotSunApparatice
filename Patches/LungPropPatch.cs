20a14dbd80929c63207a97c46f8df1915d57a4c8
Fexlar <122419606+fexlars@users.noreply.github.com>
2024-01-15 21:45:07 -0600

    Rename Apparatus to The Sun

--- Patches/LungPropPatch.cs
+++ Patches/LungPropPatch.cs
@@ -17,8 +17,15 @@ namespace OneshotSunApparatice.Patches
         {
             Plugin.Instance.mls.LogInfo("LungPropPatch.Start");
 
+            // Rename anything relating to the aparatus to be The Sun
+            ScanNodeProperties scanNodeProperties = __instance.gameObject.GetComponentInChildren<ScanNodeProperties>();
+            scanNodeProperties.headerText = "The Sun";
+            __instance.itemProperties.itemName = "The Sun";
+
+
             GameObject.Destroy(__instance.gameObject.transform.Find("Mesh").gameObject);
             GameObject gameObject = GameObject.Instantiate(Plugin.assetBundle.LoadAsset<GameObject>("assets/bulb.prefab"), __instance.gameObject.transform);
+
             // Hard Coded Positioning because I'm lazy
             gameObject.transform.localRotation = Quaternion.Euler(0, 180, 180);
             gameObject.transform.localScale = new Vector3(4, 4, 4);
